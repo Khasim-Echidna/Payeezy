363c7c6fc4e6aa1b95ac3dddb75478d3b4f36c90
ECHIDNA\khasim.a <khasim.a@echidnainc.com>
2017-07-21 20:08:18 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var test = require('./test/payeezy-test');
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.post('/Payeezy_Payment', test.performPurchaseTransaction);
+app.get('/wines/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
