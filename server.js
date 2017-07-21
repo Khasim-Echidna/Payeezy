07106f910a10fc4b72ae519b6783c56b7ed34a02
ECHIDNA\khasim.a <khasim.a@echidnainc.com>
2017-07-21 20:36:09 +0530

    Second Commit

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.get('/wines/:id', function(req, res) {
     res.send({id:req.params.id, name: "The Name", description: "description"});
 });
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
