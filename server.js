37d7f9a61dc5beb1b6d818a8f5f1a11f8e3a9e9b
ECHIDNA\khasim.a <khasim.a@echidnainc.com>
2017-07-23 12:04:00 +0530

    forth commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var test = require('./test/payeezy-test');
 var app = express();
 
 app.configure(function () {
-    //app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
 });
 
@@ -12,6 +12,6 @@ app.get('/wines/:id', function(req, res) {
     res.send({id:req.params.id, name: "The Name", description: "description"});
 });
 
-app.listen(process.env.PORT || 3320, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
